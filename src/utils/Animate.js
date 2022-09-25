f7b8516aa5b0b9bbb1198b5664b5683ffb55cce0
0b1can0b <79448678+0b1can0b@users.noreply.github.com>
2022-09-26 03:31:24 +0530

    0331

--- src/utils/Animate.js
+++ src/utils/Animate.js
@@ -35,7 +35,7 @@ const Animate = (props) => {
       setTimeout(() => {
         onEnter && onEnter();
         setEntered(true);
-      }, 0);
+      }, 500);
     } else {
       setStyleFrom(from);
       setEntered(false);
